0a661bf7b9f8aa9205ed1c656f1b7e2f5ec41d4f
jamiesun <jamiesun.net@gmail.com>
2015-01-12 13:50:44 +0800

    ...

--- createdb.py
+++ createdb.py
@@ -8,7 +8,7 @@ import argparse,json
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-c','--conf', type=str,default='./config.json',dest='conf',help='conf file')
-    parser.add_argument('-u','--update',nargs='?', type=bool,default='./config.json',dest='update',help='update option')
+    parser.add_argument('-u','--update',nargs='?', type=bool,default=False,dest='update',help='update option')
     args =  parser.parse_args(sys.argv[1:])    
     if args.update:
         models.update(config=json.loads(open(args.conf,'rb').read())['mysql'])
