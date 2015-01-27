d8a833993203734a1a3b4728def6e2f370730e29
jamiesun <jamiesun.net@gmail.com>
2015-01-28 03:11:47 +0800

    update docker script

--- docker/upgrade.sh
+++ docker/upgrade.sh
@@ -6,8 +6,10 @@ cd /opt/toughradius \
     && git pull origin master
 
 yes | cp -f /opt/toughradius/docker/startup.sh /opt/startup.sh
+yes | cp -f /opt/toughradius/docker/upgrade.sh /opt/upgrade.sh
 
 chmod +x /opt/startup.sh
+chmod +x /opt/upgrade.sh
 supervisorctl restart all
 
 echo 'upgrade ok'
