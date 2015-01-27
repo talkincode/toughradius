88773435adbe492cfbe6fe6a6f68d9e863148fae
jamiesun <jamiesun.net@gmail.com>
2015-01-28 03:20:28 +0800

    update docker script

--- docker/upgrade.sh
+++ docker/upgrade.sh
@@ -7,9 +7,14 @@ cd /opt/toughradius \
 
 yes | cp -f /opt/toughradius/docker/startup.sh /opt/startup.sh
 yes | cp -f /opt/toughradius/docker/upgrade.sh /opt/upgrade.sh
+yes | cp -f /opt/toughradius/docker/radiusd.json /var/toughradius/radiusd.json
+yes | cp -f /opt/toughradius/docker/supervisord.conf /var/toughradius/supervisord.conf
 
 chmod +x /opt/startup.sh
 chmod +x /opt/upgrade.sh
+
+supervisorctl reload
 supervisorctl restart all
+supervisorctl status
 
 echo 'upgrade done'
