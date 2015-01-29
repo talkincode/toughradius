96aa135ecff900991857dfdf2e1ad6274f72bed5
jamiesun <jamiesun.net@gmail.com>
2015-01-29 17:58:27 +0800

    auto install script

--- install/centos7-install.sh
+++ install/centos7-install.sh
@@ -6,40 +6,51 @@
 # 
 # =============================================================================
 
-echo "install lib"
-yum update -y
-yum install -y wget git gcc python-devel python-setuptools tcpdump
 
-echo "install mysql"
-rpm -ivh http://dev.mysql.com/get/mysql-community-release-el7-5.noarch.rpm
-yum install -y mysql-community-server mysql-community-devel 
+depend()
+{
+    echo "install lib"
+    yum update -y
+    yum install -y wget git gcc python-devel python-setuptools tcpdump
 
-echo "python package"
-easy_install pip 
-easy_install supervisor
+    echo "python package"
+    easy_install pip 
+    easy_install supervisor
+}
 
-echo "pull ToughRADIUS latest"
-git clone https://github.com/talkincode/ToughRADIUS.git /opt/toughradius
-pip install -r /opt/toughradius/requirements.txt
+mysql()
+{
+    echo "install mysql"
+    rpm -ivh http://dev.mysql.com/get/mysql-community-release-el7-5.noarch.rpm
+    yum install -y mysql-community-server mysql-community-devel 
+}
 
-if [ ! -d /var/toughradius ]; then
+
+radius()
+{
+    echo "pull ToughRADIUS latest"
+    git clone https://github.com/talkincode/ToughRADIUS.git /opt/toughradius
+    pip install -r /opt/toughradius/requirements.txt
+
     echo "mkdir /var/toughradius"
     mkdir -p /var/toughradius
     mkdir -p /var/toughradius/mysql
     mkdir -p /var/toughradius/log
 
-    yes | cp -f /opt/toughradius/docker/my.cnf /var/toughradius/my.cnf
+    yes | cp -f /opt/toughradius/docker/my.cnf /etc/my.cnf
     yes | cp -f /opt/toughradius/docker/radiusd.json /var/toughradius/radiusd.json
     yes | cp -f /opt/toughradius/docker/supervisord.conf /var/toughradius/supervisord.conf    
-fi
+    ln -s /opt/toughradius/install/upgrade.sh /usr/bin/radius_upgrade
+    chmod +x /usr/bin/radius_upgrade
+}
 
-if [ ! -f /var/toughradius/mysql/ibdata1 ]; then
-
+setup()
+{
     echo "starting install mysql database;"
 
     sleep 1s
 
-    mysql_install_db --defaults-file=/var/toughradius/my.cnf
+    mysql_install_db 
 
     chown -R mysql:mysql /var/toughradius/mysql
 
@@ -49,24 +60,72 @@ if [ ! -f /var/toughradius/mysql/ibdata1 ]; then
 
     echo "GRANT ALL ON *.* TO admin@'%' IDENTIFIED BY 'radius' WITH GRANT OPTION; FLUSH PRIVILEGES" | mysql
 
+    echo "setup toughradius database.."
+
     python /opt/toughradius/createdb.py -c /var/toughradius/radiusd.json -i=1
 
-    mysqladmin -uroot shutdown
-    sleep 1s
-fi
+    echo "starting supervisord..."
 
-echo "starting mysqld_safe..."
+    supervisord -c /var/toughradius/supervisord.conf
 
-/usr/bin/mysqld_safe --defaults-file=/var/toughradius/my.cnf &
+    sleep 3s
 
-sleep 5s
+    echo "ToughRADIUS service status"
 
-echo "starting supervisord..."
+    supervisorctl status
+    
+    echo "setup done!"
+}
 
-supervisord -c /var/toughradius/supervisord.conf
+unsetup()
+{
+    echo "to delete toughradius "
+    read -s -n1 -p "Press any key to continue ... "
+    echo "shutdown mysql.."
+    mysql -uroot shutdown
+    echo "shutdown supervisord"
+    supervisorctl shutdown
+    echo "/opt/toughradius"
+    rm -fr /opt/toughradius
+    echo "/var/toughradius"
+    rm -fr /var/toughradius
+    echo "/usr/bin/radius_upgrade"
+    rm -f /usr/bin/radius_upgrade
+    echo 'unsetup done!'
+}
 
-sleep 3s
 
-echo "ToughRADIUS service status"
+case "$1" in
 
-supervisorctl status
+  depend)
+    depend
+  ;;
+
+  mysql)
+    mysql
+  ;;
+
+  radius)
+    radius
+  ;;
+  
+  setup)
+    setup
+  ;;
+  
+  unsetup)
+    unsetup
+  ;;    
+  
+  all)
+    depend
+    mysql
+    radius
+    setup
+  ;;
+
+  *)
+    echo "Usage: $0 {depend|mysql|radius|setup|unsetup|all}"
+  ;;
+
+esac
