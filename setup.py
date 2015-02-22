6459df2cd9d438e3875d5da31dfbe2809ab3bf01
jamiesun <jamiesun.net@gmail.com>
2015-02-22 22:55:07 +0800

    update makefile

--- setup.py
+++ setup.py
@@ -52,7 +52,6 @@ setup(name='toughradius',
        'License :: OSI Approved :: BSD License',
        'Programming Language :: Python :: 2.6',
        'Programming Language :: Python :: 2.7',
-       "Programming Language :: Python :: Implementation :: PyPy"
        'Topic :: Software Development :: Libraries :: Python Modules',
        'Topic :: System :: Systems Administration :: Authentication/Directory',
        ],
