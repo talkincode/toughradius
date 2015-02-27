179b252ca569878917a2385c5b28b02f1234acc4
jamiesun <jamiesun.net@gmail.com>
2015-02-27 22:34:24 +0800

    #25 opr password format error

--- toughradius/console/admin/forms.py
+++ toughradius/console/admin/forms.py
@@ -104,7 +104,7 @@ opr_status_dict = {0:u'正常',1:u"停用"}
 opr_add_form = pyforms.Form(
         pyforms.Textbox("operator_name", rules.len_of(2,32), description=u"操作员名称",required="required",**input_style),
         pyforms.Textbox("operator_desc", rules.len_of(0,255),description=u"操作员姓名",**input_style),
-        pyforms.Password("operator_pass", rules.is_alphanum2(6, 128), description=u"操作员密码", required="required",**input_style),
+        pyforms.Password("operator_pass", rules.len_of(6, 128), description=u"操作员密码", required="required",**input_style),
         pyforms.Dropdown("operator_status", description=u"操作员状态", args=opr_status_dict.items(), required="required",**input_style),
         pyforms.Button("submit",  type="submit", html=u"<b>提交</b>", **button_style),
         title=u"增加操作员",
@@ -115,7 +115,7 @@ opr_update_form = pyforms.Form(
         pyforms.Hidden("id",  description=u"编号"),
         pyforms.Textbox("operator_name", description=u"操作员名称",readonly="readonly",**input_style),
         pyforms.Textbox("operator_desc", rules.len_of(0,255),description=u"操作员姓名",**input_style),
-        pyforms.Password("operator_pass", rules.is_alphanum2(0, 128), description=u"操作员密码(留空不修改)",**input_style),
+        pyforms.Password("operator_pass", rules.len_of(0, 128), description=u"操作员密码(留空不修改)",**input_style),
         pyforms.Dropdown("operator_status", description=u"操作员状态", args=opr_status_dict.items(), required="required",**input_style),
         pyforms.Button("submit",  type="submit", html=u"<b>提交</b>", **button_style),
         title=u"修改操作员",
