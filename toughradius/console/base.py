6ade12c5ac7b869e488a60b40c54633ea8b6c8c2
Administrator <sumonchai@gmail.com>
2015-05-05 00:28:14 +0700

    - _ = tr.t

--- toughradius/console/base.py
+++ toughradius/console/base.py
@@ -18,16 +18,17 @@ from beaker.cache import CacheManager
 from toughradius.console.libs.paginator import Paginator
 from toughradius.console import models
 
-from toughradius.tools import i18n
-from io import open
-
-# use the Translator class directly:
-#tr = i18n.Translator('../toughradius/console/lang.yml', language='th', fallback='en')
-#tr = i18n.Translator('../toughradius/console/lang.yml')
-# or use the load_translator() function:
-tr = i18n.load_translator('../toughradius/console/lang.yml')
-#tr.language = 'th'
-# tr.fallback = ''
+# from toughradius.console.customer import forms
+# from toughradius.tools import i18n
+# from io import open
+#
+# # use the Translator class directly:
+# #tr = i18n.Translator('../toughradius/console/lang.yml', language='th', fallback='en')
+# #tr = i18n.Translator('../toughradius/console/lang.yml')
+# # or use the load_translator() function:
+# tr = i18n.load_translator('../toughradius/console/lang.yml')
+# #tr.language = 'th'
+# # tr.fallback = ''
 # _ = tr.t
 
 ########################################################################
