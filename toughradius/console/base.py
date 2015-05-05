cdc64b951b4c38a3e69764ed9f66142dfa7395ea
Administrator <sumonchai@gmail.com>
2015-05-06 00:15:50 +0700

    Test Lang

--- toughradius/console/base.py
+++ toughradius/console/base.py
@@ -19,17 +19,17 @@ from toughradius.console.libs.paginator import Paginator
 from toughradius.console import models
 
 # from toughradius.console.customer import forms
-# from toughradius.tools import i18n
-# from io import open
+from toughradius.console.libs import i18n
+from io import open
 #
 # # use the Translator class directly:
 # #tr = i18n.Translator('../toughradius/console/lang.yml', language='th', fallback='en')
 # #tr = i18n.Translator('../toughradius/console/lang.yml')
 # # or use the load_translator() function:
-# tr = i18n.load_translator('../toughradius/console/lang.yml')
-# #tr.language = 'th'
-# # tr.fallback = ''
-# _ = tr.t
+tr = i18n.load_translator('../toughradius/console/customer/lang.yml')
+#tr.language = ''
+#tr.fallback = ''
+_ = tr.t
 
 ########################################################################
 # const define
