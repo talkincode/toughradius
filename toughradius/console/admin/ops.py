668a8d1d904694f5103845b061c31b07414d2873
jamiesun <jamiesun.net@gmail.com>
2015-06-20 00:02:27 +0800

    参数配置界面修改 api接口加入 ticket模块分离

--- toughradius/console/admin/ops.py
+++ toughradius/console/admin/ops.py
@@ -123,60 +123,6 @@ permit.add_route("%s/user/release"%__prefix__,u"用户释放绑定",u"维护管�
 
 
 ###############################################################################
-# ticket manage        
-###############################################################################
-
-@app.route('/ticket',apply=auth_opr,method=['GET','POST'])
-def ticket_query(db, render):
-    node_id = request.params.get('node_id')
-    account_number = request.params.get('account_number')  
-    framed_ipaddr = request.params.get('framed_ipaddr')  
-    mac_addr = request.params.get('mac_addr')  
-    query_begin_time = request.params.get('query_begin_time')  
-    query_end_time = request.params.get('query_end_time')  
-    opr_nodes = get_opr_nodes(db)
-    _query = db.query(
-        models.SlcRadTicket.id,
-        models.SlcRadTicket.account_number,
-        models.SlcRadTicket.nas_addr,
-        models.SlcRadTicket.acct_session_id,
-        models.SlcRadTicket.acct_start_time,
-        models.SlcRadTicket.acct_stop_time,
-        models.SlcRadTicket.acct_input_octets,
-        models.SlcRadTicket.acct_output_octets,
-        models.SlcRadTicket.acct_input_gigawords,
-        models.SlcRadTicket.acct_output_gigawords,
-        models.SlcRadTicket.framed_ipaddr,
-        models.SlcRadTicket.mac_addr,
-        models.SlcRadTicket.nas_port_id,
-        models.SlcMember.node_id,
-        models.SlcMember.realname
-    ).filter(
-            models.SlcRadTicket.account_number == models.SlcRadAccount.account_number,
-            models.SlcMember.member_id == models.SlcRadAccount.member_id
-    )
-    if node_id:
-        _query = _query.filter(models.SlcMember.node_id == node_id)
-    else:
-        _query = _query.filter(models.SlcMember.node_id.in_([i.id for i in opr_nodes]))
-    if account_number:
-        _query = _query.filter(models.SlcRadTicket.account_number.like('%'+account_number+'%'))
-    if framed_ipaddr:
-        _query = _query.filter(models.SlcRadTicket.framed_ipaddr == framed_ipaddr)
-    if mac_addr:
-        _query = _query.filter(models.SlcRadTicket.mac_addr == mac_addr)
-    if query_begin_time:
-        _query = _query.filter(models.SlcRadTicket.acct_start_time >= query_begin_time)
-    if query_end_time:
-        _query = _query.filter(models.SlcRadTicket.acct_stop_time <= query_end_time)
-
-    _query = _query.order_by(models.SlcRadTicket.acct_start_time.desc())
-    return render("ops_ticket_list", page_data = get_page_data(_query),
-               node_list=opr_nodes,**request.params)
-
-permit.add_route("%s/ticket"%__prefix__,u"上网日志查询",u"维护管理",is_menu=True,order=3)
-
-###############################################################################
 # ops log manage        
 ###############################################################################
 
