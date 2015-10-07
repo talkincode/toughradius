6905c3f3a93d07e4f49770fda236921ae74b918e
jamiesun <jamiesun.net@gmail.com>
2015-10-07 22:28:16 +0800

    clear unused import

--- toughradius/console/control/service.py
+++ toughradius/console/control/service.py
@@ -1,25 +1,11 @@
 #!/usr/bin/env python
 # coding:utf-8
-import sys, os
-from twisted.python import log
-from twisted.internet import reactor
-from bottle import Bottle
-from bottle import request
-from bottle import response
-from bottle import redirect
-from bottle import static_file
-from bottle import abort
-from hashlib import md5
-from urlparse import urljoin
-from toughradius.console.base import *
-from toughradius.console.libs import utils
-import time
-import bottle
-import decimal
-import datetime
-import functools
+import os
 import subprocess
-import platform
+
+from bottle import Bottle
+
+from toughradius.console.base import *
 
 __prefix__ = "/service"
 
