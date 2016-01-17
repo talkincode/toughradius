557662addc10170f1ed8943309795a2bfa4e3b29
jamiesun <jamiesun.net@gmail.com>
2016-01-17 18:12:36 +0800

    event settings

---
+++ toughradius/manage/events/settings.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+#coding:utf-8
+
+ACCOUNT_OPEN_EVENT = 'account_open_event'
+ACCOUNT_NEXT_EVENT = 'account_next_event'
+ACCOUNT_PAUSE_EVENT = 'account_pause_event'
+ACCOUNT_RESUME_EVENT = 'account_resume_event'
+ACCOUNT_CHANGE_EVENT = 'account_change_event'
+ACCOUNT_REFUND_EVENT = 'account_refund_event'
+ACCOUNT_CHANNEL_EVENT = 'account_cancel_event'
+
+
+SERVICE_INSTALL_EVENT = 'service_install_event'
+SERVICE_MAINTAIN_EVENT = 'service_maintain_event'
