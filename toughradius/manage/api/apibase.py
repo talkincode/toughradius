84be9d36be1126b8265a9aa3a9c555595a09941c
toughman <wjt.net@outlook.com>
2016-03-05 14:28:19 +0800

    update  api invoke, add status defines

--- toughradius/manage/api/apibase.py
+++ toughradius/manage/api/apibase.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 #coding=utf-8
 
-#!/usr/bin/env python
-# coding:utf-8
 import json
 import time
 import traceback
@@ -10,6 +8,7 @@ from hashlib import md5
 from cyclone.util import ObjectDict
 from toughlib import utils, apiutils, dispatch, logger
 from toughradius.manage.base import BaseHandler
+from toughlib.apiutils import apistatus
 
 
 class ApiHandler(BaseHandler):
@@ -44,5 +43,47 @@ class ApiHandler(BaseHandler):
         session_opr.opr_type = 0
         session_opr.login_time = utils.get_currtime()
         return session_opr
+
+
+    def _decode_msg(self,err, msg):
+        _msg = msg and utils.safeunicode(msg) or ''
+        if issubclass(type(err),BaseException):
+            return u'{0}, {1}'.format(utils.safeunicode(_msg),utils.safeunicode(err.message))
+        else:
+            return _msg
+
+    def render_success(self, msg=None, **result):
+        self.render_result(code=apistatus.success.code,
+            msg=self._decode_msg(None,msg or apistatus.success.msg),**result)
+
+    def render_sign_err(self, err=None, msg=None):
+        self.render_result(code=apistatus.sign_err.code,
+            msg=self._decode_msg(err,msg or apistatus.sign_err.msg))
+ 
+    def render_parse_err(self, err=None, msg=None):
+        self.render_result(code=apistatus.sign_err.code, 
+            msg=self._decode_msg(err,msg or apistatus.sign_err.msg))
+ 
+    def render_verify_err(self, err=None,msg=None):
+        self.render_result(code=apistatus.verify_err.code, 
+            msg=self._decode_msg(err,msg or apistatus.verify_err.msg))
+ 
+    def render_server_err(self,err=None, msg=None):
+        self.render_result(code=apistatus.server_err.code, 
+            msg=self._decode_msg(err,msg or apistatus.server_err.msg))
+
+    def render_timeout(self,err=None, msg=None):
+        self.render_result(code=apistatus.timeout.code, 
+            msg=self._decode_msg(err,msg or apistatus.timeout))
+
+    def render_limit_err(self,err=None, msg=None):
+        self.render_result(code=apistatus.limit_err.code, 
+            msg=self._decode_msg(err,msg or apistatus.limit_err)) 
+
+    def render_unknow(self,err=None, msg=None):
+        self.render_result(code=apistatus.unknow.code, 
+            msg=self._decode_msg(err,msg or apistatus.unknow))
+
+
 
 
