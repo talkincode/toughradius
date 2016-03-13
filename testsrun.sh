c84cdc7da22316966cdeb026d595de34dfa91e71
toughman <wjt.net@outlook.com>
2016-03-13 19:03:03 +0800

    update tests

--- testsrun.sh
+++ testsrun.sh
@@ -2,10 +2,10 @@
 
 python toughctl --initdb -c toughradius/tests/test.json
 
-python coverage run toughctl --standalone -exitwith 120 -c toughradius/tests/test.json &
+python coverage run toughctl --standalone -exitwith 60 -c toughradius/tests/test.json &
 
-sleep 15
+sleep 10
 
 python trial toughradius.tests
 
-
+sleep 50
