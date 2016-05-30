7398e2d6f4bf34d18befb5e87cba2edf1fac3f05
jamiesun <jamiesun.net@gmail.com>
2016-05-30 14:50:17 +0800

    2.1.6 - 2016-05-30 14:49:58: update sid method

--- toughradius/common/tools.py
+++ toughradius/common/tools.py
@@ -7,16 +7,12 @@ from hashlib import md5
 from twisted.internet import utils as txutils
 
 def get_sys_uuid():
-    fs = '/sys/class/dmi/id/product_uuid'
-    if os.path.exists(fs):
-        _file = open(fs)
-        _uuid = _file.read()
-        _file.close()
-        return md5(_uuid).hexdigest()
-    else:
-        mac=uuid.UUID(int = uuid.getnode()).hex[-12:] 
-        return md5(":".join([mac[e:e+2] for e in range(0,11,2)])).hexdigest()
-    return md5('free-uuid').hexdigest()
+    mac=uuid.UUID(int = uuid.getnode()).hex[-12:] 
+    return md5(":".join([mac[e:e+2] for e in range(0,11,2)])).hexdigest()
+
 
 def get_sys_token():
     return txutils.getProcessOutput("/usr/local/bin/toughkey")
+
+if __name__ == '__main__':
+    print get_sys_uuid()
