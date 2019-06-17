a07d314174c49a10010fb1593a8fa5354cbb9dd9
jamiesun <jamiesun.net@gmail.com>
2019-06-17 19:20:44 +0800

    lf

--- release.sh
+++ release.sh
@@ -13,12 +13,17 @@ build_version()
     test -f ${releasefile} && rm -f ${releasefile}
 
     cp ${currdir}/scripts/application-prod.properties ${releasedir}/application-prod.properties
+
     cp ${currdir}/scripts/createdb.sql ${releasedir}/createdb.sql
     cp ${currdir}/scripts/database.sql ${releasedir}/database.sql
     cp ${currdir}/scripts/init.sql ${releasedir}/init.sql
     cp ${currdir}/scripts/installer.sh ${releasedir}/installer.sh
-    cp ${currdir}/scripts/startup.bat ${releasedir}/startup.bat
     cp ${currdir}/scripts/toughradius.service ${releasedir}/toughradius.service
+    dos2unix ${releasedir}/*.properties
+    dos2unix ${releasedir}/*.sql
+    dos2unix ${releasedir}/*.sh
+    dos2unix ${releasedir}/*.service
+    cp ${currdir}/scripts/startup.bat ${releasedir}/startup.bat
     cp ${currdir}/target/toughradius-latest.jar ${releasedir}/toughradius-latest.jar
     cd ${currdir}/release && zip -r ${releasefile} toughradius-${version}
     echo "release file ${releasefile}"
