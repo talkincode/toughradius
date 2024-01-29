c01bac5295d59faa3e7ccb5ca2432837476523a0
Jett Wang <jamiesun.net@gmail.com>
2024-01-29 21:40:25 +0800

    Update translations and database privileges

--- assets/createdb.sql
+++ assets/createdb.sql
@@ -1,3 +1,3 @@
 CREATE USER toughradius WITH PASSWORD 'toughradius';
 CREATE DATABASE toughradius WITH OWNER toughradius;
-GRANT ALL PRIVILEGES ON DATABASE toughradius_v8 TO toughradius;
+GRANT ALL PRIVILEGES ON DATABASE toughradius TO toughradius;
